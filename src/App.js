56667b9f8acb5298d26874b1fc4016ab47065075
Charlotte Prevost <chaprevost91@gmail.com>
2018-10-31 13:36:58 -0500

    Sorry totally forgot - Lists quakes with place - mag - hours since

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
+import Map from './Map';
+import Quakes from './Quakes';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      earthquakes: []
+    }
+  }
+
+
+  getEarthQuakes = async () => {
+    try{
+
+      const earthquakes     = await fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson');
+      const earthquakesJson = await earthquakes.json();
+      return earthquakesJson;
+
+    } catch(err){
+      console.error(`Error: `, err);
+      return err;
+    }
+  }
+
+  componentDidMount(){
+
+    this.getEarthQuakes().then(data=>{
+
+      this.setState({earthquakes: data.features})
+
+    }).catch(err=> console.error(`Error: `, err))
+
+  }
 
 
   render() {
+    console.log(`this.state.earthquakes: `, this.state.earthquakes);
     return (
       <div className="app">
         <div className="mapContainer">
           ...put Map Component here...
+          <Map />
         </div>
         <div className="quakeContainer">
           <h1>Earthquakes from the past week: </h1>
           ...put Quakes Component here...
+          <Quakes quakes={this.state.earthquakes}/>
         </div>
       </div>
     );
---
+++ src/Quakes/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+
+class Quakes extends Component {
+    render(){
+					// let date = new Date(quake.properties.time*1000);
+					// let hours = date.getHours();
+					// let minutes = "0" + date.getMinutes();
+					// let seconds = "0" + date.getSeconds();
+					// let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+					// console.log(formattedTime);
+					// return formattedTime;
+					// }
+
+    	const quakeList = this.props.quakes.map((quake, i) => {
+    		const dateNow = Date.now();
+
+    		let hoursElapsed = Math.abs(dateNow - quake.properties.time)/36e5;
+    		console.log(`hoursElapsed: `, hoursElapsed);
+
+    		return(
+    			<li key={i}>
+    				Title: {quake.properties.title}<br/>
+    				Mag: {quake.properties.mag}<br/>
+    				Hours Since: {Math.floor(hoursElapsed)}<br/>
+    			</li>
+    		)
+    	})
+
+        return(
+            <div>
+            	<h1>Quakes</h1>
+            	<ul>{quakeList}</ul>
+            </div>
+        )
+    }
+}
+export default Quakes;
